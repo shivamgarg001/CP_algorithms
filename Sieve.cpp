99ed5de54f231e56bb8ad5869a2c8d203dc15ee6
shivamgarg001 <142019041+shivamgarg001@users.noreply.github.com>
2024-06-19 12:26:54 +0530

    Add files via upload

--- Sieve.cpp
+++ Sieve.cpp
@@ -1,32 +1,20 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-#define int long long
 
-vector<bool> vec(90000001, true);
-vector<int> ans;
-
-void pre_cal(){
-    for (int i = 2; i <= 90000000; i++)
+int main(){
+    int n;
+    cin>>n;
+    int arr[n+1];
+    for(int i=0;i<=n;i++){
+        arr[i]=1;
+    }
+    arr[0]=arr[1]=0;
+    for (int i=2;i<=n;i++)
     {
-        if (vec[i] == true){
-            ans.push_back(i);
-            for(int j=i*i;j<=90000001; j+=i)    vec[j]=false;
+        if (arr[i] == 1){
+            cout<<i<<" ";
+            for(int j=i*i;j<=n; j+=i)    arr[j]=0;
         }
     }
-}
-
-int32_t main(){
-    int q;
-    cin >> q;
-    pre_cal();
-
-    while (q--)
-    {
-        int n;
-        cin >> n;
-        cout << ans[n - 1] << endl;
-    }
-
     return 0;
 }
-
